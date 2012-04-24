grnet/kamaki

commit 31231dee4fdd234c0ccda3b293fc5f2602a6200c

Do not read more than the given size

kamaki/clients/pithos.py
@@ -80,30 +80,30 @@ class PithosClient(StorageClient):
         meta = self.get_container_meta(self.container)
         blocksize = int(meta['block-size'])
         blockhash = meta['block-hash']
-        
-        file_size = size if size is not None else os.fstat(f.fileno()).st_size
+
+        size = size if size is not None else os.fstat(f.fileno()).st_size
         nblocks = 1 + (file_size - 1) // blocksize
         hashes = []
         map = {}
 
-        size = 0
-        
+        offset = 0
+
         if hash_cb:
             hash_gen = hash_cb(nblocks)
             hash_gen.next()
-    
+
         for i in range(nblocks):
-            block = f.read(blocksize)
+            block = f.read(min(blocksize, size - offset))
             bytes = len(block)
             hash = pithos_hash(block, blockhash)
             hashes.append(hash)
-            map[hash] = (size, bytes)
-            size += bytes
+            map[hash] = (offset, bytes)
+            offset += bytes
             if hash_cb:
                 hash_gen.next()
-        
-        assert size == file_size
-                
+
+        assert offset == size
+
         path = '/%s/%s/%s' % (self.account, self.container, object)
         params = dict(format='json', hashmap='')
         hashmap = dict(bytes=size, hashes=hashes)

commit 4e1a34e98059b239858135a8f5bb3538904118e1

Fix `var not defined' bug introduced in 31231dee4f

kamaki/clients/pithos.py
@@ -82,7 +82,7 @@ class PithosClient(StorageClient):
         blockhash = meta['block-hash']
 
         size = size if size is not None else os.fstat(f.fileno()).st_size
-        nblocks = 1 + (file_size - 1) // blocksize
+        nblocks = 1 + (size - 1) // blocksize
         hashes = []
         map = {}
 