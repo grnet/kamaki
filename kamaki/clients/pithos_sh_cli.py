98893eac74ca4db2bc5348a625403b1dbbafd957
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2012-08-23 12:03:48 +0300

    Add sharers from pithos_sh
    sharers shows the accounts that share objects with cur account

--- kamaki/clients/pithos_sh_cli.py
+++ kamaki/clients/pithos_sh_cli.py
@@ -34,8 +34,9 @@
 # or implied, of GRNET S.A.
 
 from kamaki.cli import command, CLIError
+from kamaki.utils import print_list
 from .utils import dict2file, list2file
-from .pithos_cli import _store_container_command
+from .pithos_cli import _store_container_command, _store_account_command
 
 #from getpass import getuser
 #from optparse import OptionParser
@@ -54,6 +55,15 @@ import types
 #import time as _time
 #import os
 
+class _pithos_sh_account_command(_store_account_command):
+
+    def update_parser(self, parser):
+        super(_pithos_sh_account_command, self).update_parser(parser)
+
+    def main(self):
+        super(_pithos_sh_account_command, self).main()
+        self.client = Pithos_Client(self.base_url, self.token, self.account)
+
 class _pithos_sh_container_command(_store_container_command):
 
     def update_parser(self, parser):
@@ -63,7 +73,6 @@ class _pithos_sh_container_command(_store_container_command):
         super(_pithos_sh_container_command, self).main(container_with_path, path_is_optional)
         self.client = Pithos_Client(self.base_url, self.token, self.account)
 
-  
 def _build_args(arglist, attrs):
     args = {}
     for a in [a for a in attrs if getattr(arglist, a)]:
@@ -142,211 +151,23 @@ class store_download(_pithos_sh_container_command):
             f.write(unicode(data)+'\n')
         f.close()
 
-"""
 @command()
-class store_CopyObject(object):
-    ""cop an object to a different location""
-    syntax = '<src container>/<src object> [<dst container>/]<dst object> [key=val] [...]'
-
-    def main(self):
-        pass
-    
-    def add_options(self, parser):
-        parser.add_option('--version', action='store',
-                          dest='version', default=False,
-                          help='copy specific version')
-        parser.add_option('--public', action='store_true',
-                          dest='public', default=False,
-                          help='make object publicly accessible')
-        parser.add_option('--content-type', action='store',
-                          dest='content_type', default=None,
-                          help='change object\'s content type')
-        parser.add_option('--delimiter', action='store', 
-                          dest='delimiter', default=None,
-                          help='mass copy objects with path staring with <src object> + delimiter')
-        parser.add_option('-r', action='store_true',
-                          dest='recursive', default=False,
-                          help='mass copy with delimiter /')
-    
-    def execute(self, src, dst, *args):
-        src_container, sep, src_object = src.partition('/')
-        dst_container, sep, dst_object = dst.partition('/')
-        
-        #prepare user defined meta
-        meta = {}
-        for arg in args:
-            key, sep, val = arg.partition('=')
-            meta[key] = val
-        
-        if not sep:
-            dst_container = src_container
-            dst_object = dst
-        
-        args = {'content_type':self.content_type} if self.content_type else {}
-        if self.delimiter:
-        	args['delimiter'] = self.delimiter
-        elif self.recursive:
-        	args['delimiter'] = '/'
-        self.client.copy_object(src_container, src_object, dst_container,
-                                dst_object, meta, self.public, self.version,
-                                **args)
-
-@command()
-class store_UpdateObject(object):
-    ""update object metadata/data (default mode: append)""
-    syntax = '<container>/<object> path [key=val] [...]'
-
-    def main(self):
-        pass
+class store_sharers(_pithos_sh_account_command):
+    """list accounts who share objects with current account"""
     
     def update_parser(self, parser):
-        super(store_UpdateObject, self).update_parser(parser)
-        parser.add_argument('-a', action='store_true', dest='append',
-                          default=True, help='append data')
-        parser.add_argument('--offset', action='store',
-                          dest='offset',
-                          default=None, help='starting offest to be updated')
-        parser.add_argument('--range', action='store', dest='content_range',
-                          default=None, help='range of data to be updated')
-        parser.add_argument('--chunked', action='store_true', dest='chunked',
-                          default=False, help='set chunked transfer mode')
-        parser.add_argument('--content-encoding', action='store',
-                          dest='content_encoding', default=None,
-                          help='provide the object MIME content type')
-        parser.add_argument('--content-disposition', action='store', 
-                          dest='content_disposition', default=None,
-                          help='provide the presentation style of the object')
-        parser.add_argument('--manifest', action='store', 
-                          dest='x_object_manifest', default=None,
-                          help='use for large file support')        
-        parser.add_argument('--sharing', action='store',
-                          dest='x_object_sharing', default=None,
-                          help='define sharing object policy')
-        parser.add_argument('--nosharing', action='store_true',
-                          dest='no_sharing', default=None,
-                          help='clear object sharing policy')
-        parser.add_argument('-f', action='store',
-                          dest='srcpath', default=None,
-                          help='file descriptor to read from: pass - for standard input')
-        parser.add_argument('--public', action='store_true',
-                          dest='x_object_public', default=False,
-                          help='make object publicly accessible')
-        parser.add_argument('--replace', action='store_true',
-                          dest='replace', default=False,
-                          help='override metadata')
-    
-    def execute(self, path, *args):
-        if path.find('=') != -1:
-            raise Fault('Missing path argument')
+        super(store_sharers, self).update_parser(parser)
+        parser.add_argument('-l', action='store_true', dest='detail', default=False,
+            help='show detailed output')
+        parser.add_argument('-n', action='store',  dest='limit', default=10000,
+            help='show limited output')
+        parser.add_argument('--marker', action='store', dest='marker', default=None,
+            help='show output greater then marker')
         
-        #prepare user defined meta
-        meta = {}
-        for arg in args:
-            key, sep, val = arg.partition('=')
-            meta[key] = val
-        
-        
-        attrs = ['content_encoding', 'content_disposition', 'x_object_sharing',
-                 'x_object_public', 'x_object_manifest', 'replace', 'offset',
-                 'content_range']
-        args = self._build_args(attrs)
-        
-        if self.no_sharing:
-            args['x_object_sharing'] = ''
-        
-        container, sep, object = path.partition('/')
-        
-        f = None
-        if self.srcpath:
-            f = open(self.srcpath) if self.srcpath != '-' else stdin
-        
-        if self.chunked:
-            self.client.update_object_using_chunks(container, object, f,
-                                                    meta=meta, **args)
-        else:
-            self.client.update_object(container, object, f, meta=meta, **args)
-        if f:
-            f.close()
-
-@command()
-class store_MoveObject(object):
-    ""move an object to a different location""
-    syntax = '<src container>/<src object> [<dst container>/]<dst object>'
-
     def main(self):
-        pass
-    
-    def update_parser(self, parser):
-        super(store_MoveObject, self).update_parser(parser)
-        parser.add_argument('--public', action='store_true',
-                          dest='public', default=False,
-                          help='make object publicly accessible')
-        parser.add_argument('--content-type', action='store',
-                          dest='content_type', default=None,
-                          help='change object\'s content type')
-        parser.add_argument('--delimiter', action='store', 
-                          dest='delimiter', default=None,
-                          help='mass move objects with path staring with <src object> + delimiter')
-        parser.add_argument('-r', action='store_true',
-                          dest='recursive', default=False,
-                          help='mass move objects with delimiter /')
-    
-    def execute(self, src, dst, *args):
-        src_container, sep, src_object = src.partition('/')
-        dst_container, sep, dst_object = dst.partition('/')
-        if not sep:
-            dst_container = src_container
-            dst_object = dst
-        
-        #prepare user defined meta
-        meta = {}
-        for arg in args:
-            key, sep, val = arg.partition('=')
-            meta[key] = val
-        
-        args = {'content_type':self.content_type} if self.content_type else {}
-        if self.delimiter:
-        	args['delimiter'] = self.delimiter
-        elif self.recursive:
-        	args['delimiter'] = '/'
-        self.client.move_object(src_container, src_object, dst_container,
-                                dst_object, meta, self.public, **args)
-
-@command()
-class store_SharingObject(object):
-    ""list user accounts sharing objects with the user""
-    syntax = 'list users sharing objects with the user'
-    
-    def main(self):
-        pass
-    
-    def update_parser(self, parser):
-        super(store_SharingObject, self).update_parser(parser)
-        parser.add_argument('-l', action='store_true', dest='detail',
-                          default=False, help='show detailed output')
-        parser.add_argument('-n', action='store',  dest='limit',
-                          default=10000, help='show limited output')
-        parser.add_argument('--marker', action='store', 
-                          dest='marker', default=None,
-                          help='show output greater then marker')
-        
-    
-    def execute(self):
+        super(store_sharers, self).main()
         attrs = ['limit', 'marker']
-        args = self._build_args(attrs)
-        args['format'] = 'json' if self.detail else 'text'
+        args = _build_args(self.args, attrs)
+        args['format'] = 'json' if getattr(self.args, 'detail') else 'text'
         
         print_list(self.client.list_shared_by_others(**args))
-
-@command()
-class store_Receive(object):
-    ""download object to file""
-    syntax = '<container>/<object> <file>'
-    
-    def main(self):
-        pass
-
-    def execute(self, path, file):
-        container, sep, object = path.partition('/')
-        download(self.client, container, object, file)
-"""
