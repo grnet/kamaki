34e4cf619a4346cabab57c9abc525253f91430b0
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2012-12-19 12:38:55 +0200

    Run ignores self in command listing

--- kamaki/cli/history.py
+++ kamaki/cli/history.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2012 GRNET S.A. All rights reserved.
+# Copyright 2012-2013 GRNET S.A. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or
 # without modification, are permitted provided that the following
@@ -32,14 +32,6 @@
 # documentation are those of the authors and should not be
 # interpreted as representing official policies, either expressed
 # or implied, of GRNET S.A.
-
-
-def order_free_contains(containing, contained):
-    superset = containing.split()
-    for term in contained.split():
-        if term not in superset:
-            return False
-    return True
 
 
 class History(object):
@@ -84,7 +76,7 @@ class History(object):
             return None
         with open(self.filepath) as f:
             try:
-                lala = f.readlines()
-                return lala[cmd_id - (1 if cmd_id > 0 else 0)]
+                cmd_list = f.readlines()[:-1]  # exclude current command
+                return cmd_list[cmd_id - (1 if cmd_id > 0 else 0)]
             except IndexError:
                 return None
