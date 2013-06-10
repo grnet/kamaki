e9680623a9de7edcdfd7fe9440fde9027ea482f0
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2013-06-10 15:11:25 +0300

    Fix PEP8 warning

--- kamaki/cli/commands/image.py
+++ kamaki/cli/commands/image.py
@@ -85,10 +85,10 @@ class _init_image(_command_init):
                 return
         if getattr(self, 'auth_base', False):
             plankton_endpoints = self.auth_base.get_service_endpoints(
-                self._custom_type('image')\
-                or self._custom_type('plankton') or 'image',
-                self._custom_version('image')\
-                or self._custom_version('plankton') or '')
+                self._custom_type('image') or self._custom_type(
+                    'plankton') or 'image',
+                self._custom_version('image') or self._custom_version(
+                    'plankton') or '')
             base_url = plankton_endpoints['publicURL']
             token = self.auth_base.token
         else:
