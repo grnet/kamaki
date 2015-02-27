grnet/kamaki

commit 41e4d5b544276fd783e363a7019287cba58d0074

Fix makedirs() usage
In we run:

  os.makedirs("dir1")
  os.makedirs("dir1")

we get OSError: [Errno 17] File exists: 'dir1'

Contrary to that if we run:

  os.makedirs("dir1")
  os.makedirs("dir1/dir2")

it succeeds.

Therefore we must check if our target directory exists before
running makedirs.

Signed-off-by: Dimitris Aragiorgis <dimitris.aragiorgis@gmail.com>

kamaki/cli/cmds/pithos.py
@@ -1381,7 +1381,7 @@ class file_download(_PithosContainer):
         progress_bar = None
         try:
             for rpath, output_file in self._src_dst(local_path):
-                if not rpath:
+                if not rpath and not path.exists(output_file):
                     self.error('Create local directory %s' % output_file)
                     makedirs(output_file)
                     continue

commit fb488e4a4cd5d22a5c4a4872c4dffc16b530bb09

Fix download of an existing file
Commit 41e4d5b introduced a bug that was triggered when downloading
an existing file. Fix this.

Signed-off-by: Dimitris Aragiorgis <dimitris.aragiorgis@gmail.com>

kamaki/cli/cmds/pithos.py
@@ -1381,11 +1381,17 @@ class file_download(_PithosContainer):
         self.client.MAX_THREADS = int(self['max_threads'] or 5)
         progress_bar = None
         try:
+            # From _src_dst():
+            # If rpath is None output_file is a directory.
+            # If rpath is not None output_file is a file descriptor.
             for rpath, output_file in self._src_dst(local_path):
-                if not rpath and not path.exists(output_file):
-                    self.error('Create local directory %s' % output_file)
-                    makedirs(output_file)
+                # Create a directory
+                if not rpath:
+                    if not path.exists(output_file):
+                        self.error('Create local directory %s' % output_file)
+                        makedirs(output_file)
                     continue
+                # Download a file
                 self.error('/%s/%s --> %s' % (
                     self.container, rpath, output_file.name))
                 progress_bar, download_cb = self._safe_progress_bar(