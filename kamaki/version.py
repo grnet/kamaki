1e0729549ba4068900e8a27a041c8980528e9dd6
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2015-05-04 14:29:33 +0300

    Update Changelog and version.py for 0.13.3

--- kamaki/version.py
+++ kamaki/version.py
@@ -1,8 +1,8 @@
 
-__version__ = "0.13.2"
+__version__ = "0.13.3"
 __version_vcs_info__ = {
-    'branch': 'hotfix-0.13.2',
-    'revid': '5e5cd6a',
-    'revno': 2495}
-__version_user_email__ = "skalkoto@grnet.gr"
-__version_user_name__ = "Nikos Skalkotos"
+    'branch': 'hotfix-0.13.3',
+    'revid': '867614c',
+    'revno': 2501}
+__version_user_email__ = "saxtouri@admin.grnet.gr"
+__version_user_name__ = "Stavros Sachtouris"
