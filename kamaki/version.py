a8472c13f2505712e081bad5c6a15e4a1221e6ee
Giorgos Korfiatis <gkorf@grnet.gr>
2016-03-11 16:19:04 +0200

    Update version.py

--- kamaki/version.py
+++ kamaki/version.py
@@ -1,8 +1,8 @@
 
-__version__ = "0.14rc2"
+__version__ = "0.14rc3"
 __version_vcs_info__ = {
     'branch': 'release-0.14',
-    'revid': '8cf9245',
-    'revno': 2577}
-__version_user_email__ = "skalkoto@grnet.gr"
-__version_user_name__ = "Nikos Skalkotos"
+    'revid': 'ad20ae9',
+    'revno': 2583}
+__version_user_email__ = "gkorf@grnet.gr"
+__version_user_name__ = "Giorgos Korfiatis"
