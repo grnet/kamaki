5d1627808430c16f1176d2819a96604781b031f0
Nikos Skalkotos <skalkoto@grnet.gr>
2016-05-17 16:09:35 +0300

    Fix version.py for 0.14

--- kamaki/version.py
+++ kamaki/version.py
@@ -1,8 +1,8 @@
 
-__version__ = "0.14_2587_a3bb8e3"
+__version__ = "0.14"
 __version_vcs_info__ = {
     'branch': 'master',
-    'revid': 'a3bb8e3',
-    'revno': 2587}
+    'revid': '8d8f3b8',
+    'revno': 2589}
 __version_user_email__ = "skalkoto@grnet.gr"
 __version_user_name__ = "Nikos Skalkotos"
