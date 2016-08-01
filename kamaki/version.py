5826592f8348546913d34e062bd5a1fe20f90c68
Nikos Skalkotos <skalkoto@grnet.gr>
2016-08-01 11:13:36 +0300

    Bump development version to 0.15
    Change version to comply with devflow v0.13

--- kamaki/version.py
+++ kamaki/version.py
@@ -1,8 +1,3 @@
 
-__version__ = "0.14next"
-__version_vcs_info__ = {
-    'branch': 'develop',
-    'revid': '2eeaada',
-    'revno': 2596}
-__version_user_email__ = "skalkoto@grnet.gr"
-__version_user_name__ = "Nikos Skalkotos"
+__version__ = "0.15.dev0"
+
